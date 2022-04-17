17060bea29eebbc28c0e3e6e7735d0a3f263b1ca
YasharHTML <85059801+YasharHTML@users.noreply.github.com>
2022-04-17 18:53:00 +0400

    7380fc06308e074c1a2bf44bd2650dfb297a0bfec2a575ba6df8bf21b7be657d5931ca1c070e6539d323f6c81ef0cdacf73b6518054aca18dde4963cac663bd1 59f3de5c53d51e334976f7f8b9e7c28fbfa41e70f20a97511b45b7f128ab04d266453220a27528c99a6ba10ba9bcbf70ae24121f89276c76f6949b5157d56d7d 3df6ae78e21a0ab9809fea528b5c88955d6682dc5648424b13a2cd3b5ae81cbe5361be0f4c69de073cfce6b463f3eff1395a30354e97411483169f8767add828 1a38fedb5e12e63e3649991ce542b0a7502bbe0378763e55179c7c87d45911fb128475fdddd567b4c5b01f1aab85111c52a66ff1442317c25fb5decf74154599 e64f38caeac9d28a5cbc9e1103ba14bd0f5316e9a050d46ec99d78f94ef8b007208eb7c0017e6cda9cc02a2a9afb8440475b1e03fac64be9f5cde6ae8c419b0f

--- api_consts.dart
+++ api_consts.dart
@@ -1,6 +1,6 @@
 class ApiConst {
   ApiConst._();
-  static const String version = "v1";
+  static const String version = "v1.0.0";
   static const String indentifier = "api";
   static const String domain = "example.com";
 }
